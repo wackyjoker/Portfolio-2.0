c667399cc1137fc89763fd2f0512a0eea01b159f
wackyjoker <kevin.young8931@gmail.com>
2021-09-27 00:30:38 +0200

    adding stylelint rules

--- Components/Loader/Loader.tsx
+++ Components/Loader/Loader.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import styles from './Loader.module.scss'
 
 const Loader: React.FunctionComponent = () => (
-  <>
+
+  <div className={styles.loaderContainer}>
     <div className={styles.loader} />
     <p>Loading</p>
-  </>
+  </div>
 )
 
 export default Loader
