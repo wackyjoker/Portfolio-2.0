9952f49e7a938dd6393568373052b318de6feb43
wackyjoker <kevin.young8931@gmail.com>
2021-10-01 22:59:26 +0200

    setting up loading effect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Router from 'next/router'
+import { useState } from 'react'
+import Loader from '../components/Loader'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = useState(false)
+  Router.events.on('routeChangeStart', () => {
+    console.log('router is changing')
+    setLoading(true)
+  })
+  Router.events.on('routeChangeComplete', () => {
+    console.log('router is completed')
+    setLoading(false)
+  })
+  return (
+    <>
+      {loading && <Loader />}
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
