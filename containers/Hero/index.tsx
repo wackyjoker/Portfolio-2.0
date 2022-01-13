45566cd1e4271ed500b2ade916c2384318588e93
wackyjoker <kevin.young8931@gmail.com>
2022-01-13 05:52:33 +0100

    adding temp hero text

--- containers/Hero/index.tsx
+++ containers/Hero/index.tsx
@@ -1,6 +1,15 @@
+import { Text } from '@chakra-ui/react'
+
 const Hero = () => (
   <>
-    <div>hello</div>
+    <Text
+      bgGradient="linear(to-l, #7928CA, #FF0080)"
+      bgClip="text"
+      fontSize="6xl"
+      fontWeight="extrabold"
+    >
+      Welcome to My Portfolio
+    </Text>
   </>
 )
 export default Hero
