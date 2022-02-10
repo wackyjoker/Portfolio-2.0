6ef8855d411f09698435c60ed5e42b10573867ee
wackyjoker <kevin.young8931@gmail.com>
2022-02-10 17:23:41 +0100

    removing the logo

--- Components/Footer/footer.tsx
+++ Components/Footer/footer.tsx
@@ -1,16 +1,11 @@
-import { Link, Box, Stack } from '@chakra-ui/react'
+import { Box, Stack } from '@chakra-ui/react'
 import * as React from 'react'
 import Copyright from '@/components/Copyright'
-import Logo from '@/components/./Logo'
-// import { SocialMediaLinks } from './SocialMediaLinks'
 
 const Footer = () => (
   <Box as="footer" role="contentinfo" mx="auto" maxW="7xl" py="12" px={{ base: '4', md: '8' }}>
     <Stack>
-      <Stack direction="row" spacing="4" align="center" justify="space-between">
-        <Logo />
-        
-      </Stack>
+      <Stack direction="row" spacing="4" align="center" justify="space-between" />
       <Copyright alignSelf={{ base: 'center', sm: 'start' }} />
     </Stack>
   </Box>
